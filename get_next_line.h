d72bfe7b23483b0d78072dee0f3849a4cc19f25a
Luca Van tichelen <luca_van_tic@hotmail.com>
2022-02-02 13:25:45 +0100

    GetNextLine project passed with 112%

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvan-tic <lvan-tic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/11 09:56:17 by lvan-tic          #+#    #+#             */
+/*   Updated: 2021/07/19 12:58:01 by lvan-tic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*check_input(int fd);
+int		find_newline(char *temp);
+char	*return_string(char *temp);
+char	*move_front(char *temp);
+char	*get_next_line(int fd);
+char	*free_string(char *str);
+char	*concat_temp(char *buff, char *temp);
+char	*concat(char *buff, char *temp);
+char	*return_line(char **line, char **temp);
+
+#endif
